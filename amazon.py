25c84fb7f97de12dffeca48ffd5b61e255da15d4
Thom Hurks <thomhurks@gmail.com>
2015-10-26 18:45:11 +0100

    First commit

---
+++ amazon.py
@@ -0,0 +1,17 @@
+__author__ = 'thom'
+
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+print('Default region:')
+for instance in ec2.instances.all():
+    print(instance.id)
+
+instances = ec2.instances.filter(
+    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+for instance in instances:
+    print(instance.id, instance.instance_type)
+
+for status in ec2.meta.client.describe_instance_status()['InstanceStatuses']:
+    print(status)
