696f9be8a8ddcc4092a8f740551a9737f357244e
Grabot <SanderKools@gmail.com>
2015-10-27 17:09:22 +0100

    get status every minute

--- amazon.py
+++ amazon.py
@@ -2,15 +2,17 @@ __author__ = 'Thom Hurks'
 
 import boto3
 import os
+import time
 
 ec2 = boto3.resource('ec2')
+import datetime
 
 def getStatus():
     i = 0
     opts = {}
     for inst in instances:
         opts[i] = (inst.id, inst.instance_type, inst.public_dns_name, inst.launch_time)
-        print("\t%d:" % i + " %s - %s : %s (Running since: %s)" % opts[i])
+        print('current time: ' + str(datetime.datetime.now()) + '  instance: ' + "%d:" % i + " %s - %s : %s (Running since: %s)" % opts[i])
         i += 1
 
 
@@ -20,14 +22,14 @@ for instance in ec2.instances.all():
 
 instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 
-
 for instance in instances:
     print(instance.id, instance.instance_type)
-
 
 for status in ec2.meta.client.describe_instance_status()['InstanceStatuses']:
     print(status)
 
-
 print("\nCurrently running instances:")
-getStatus()
+
+while True:
+        getStatus()
+        time.sleep(60)
